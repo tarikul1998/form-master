c1a310db5c6dfcf4bd90efcae6561793261d5d55
Md Tarikul Islam <tarikulnayan1998@gmail.com>
2026-01-08 19:12:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+import StateFulForm from './components/StateFulForm/StateFulForm'
+
+function App() {
+
+  return (
+    <>
+      {/* <SimpleForm /> */}
+      <StateFulForm />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/StateFulForm/StateFulForm.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const StateFulForm = () => {
+    const [name, setName] = useState('Rashed');
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [error, setError] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (password.length < 6) {
+            setError('Password must be 6 characters or longer');
+        } else {
+            setError('');
+        }
+        console.log(name, email, password);
+    }
+
+    const handleNameChange = e => {
+        setName(e.target.value);
+    }
+
+    const handleEmailChange = e => {
+        // console.log(e.target.value);
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input value={name} onChange={handleNameChange}
+                    type="text" name="name" />
+                <br />
+                <input onChange={handleEmailChange}
+                    type="email" name="email" />
+                <br />
+                <input onChange={handlePasswordChange}
+                    type="password" name="password" required />
+                <br />
+                <input type="submit" value="Submit" />
+                {
+                    error && <p>{error}</p>
+                }
+            </form>
+        </div>
+    );
+};
+
+export default StateFulForm;
